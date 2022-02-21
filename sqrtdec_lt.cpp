f466bb17cf7be63920da639e718ce48a44f95231
Rudra Pratap Singh <99959625+chainsn4tchers@users.noreply.github.com>
2022-02-21 18:58:36 +0530

    Update sqrtdec_lt.cpp

--- sqrtdec_lt.cpp
+++ sqrtdec_lt.cpp
@@ -1,36 +1,83 @@
+#include <cmath>
 #include <vector>
 using namespace std;
 
-#define sz(x) (int)(x).size()
-#define ll long long
+#define ll int64_t
+#define __INT64_MIN__ -0x7fffffffffffffff - 1
 
 struct sqrtdec {
-    size_t size;
-    vector<ll> tree;
-    int identity = 0;
-
-    // holds range sum by default
-    ll init(int lo, int hi, int idx, vector<ll>& a)
-    {
-        if (lo == hi)
-            return tree[idx] = a[lo];
-
-        int mid = lo + ((hi - lo) / 2);
-        return tree[idx] = init(lo, mid, (2 * idx) + 1, a) + init(mid + 1, hi, (2 * idx) + 2, a);
-    }
+    size_t blockSize;
+    vector<ll> arr;
+    vector<ll> sumArr;
+    vector<ll> maxArr;
 
     void build(vector<ll>& a)
     {
-        init(0, sz(a) - 1, 0, a);
+        blockSize = ceil(sqrt(a.size() + .0));
+        arr = a;
+        sumArr.assign(blockSize, (ll)0);
+        maxArr.assign(blockSize, __INT64_MIN__);
+
+        for (int i = 0; i < (int)a.size(); ++i) {
+            sumArr[i / blockSize] += a[i];
+            maxArr[i / blockSize] = max(a[i], maxArr[i / blockSize]);
+        }
     }
 
-    ll rangeSum(int lo, int hi, int idx, int l, int r)
+    ll rangeSum(int l, int r)
     {
-        if (l > r)
-            return identity;
-        if (lo == l && hi == r)
-            return tree[idx];
-        int mid = lo + ((hi - lo) / 2);
-        return rangeSum(lo, mid, (2 * idx) + 1, l, r) + rangeSum(mid + 1, hi, (2 * idx) + 2, l, r);
+        ll sum = (ll)0;
+        int lblk = l / blockSize;
+        int rblk = r / blockSize;
+    
+        if (lblk == rblk) {
+            for (int i = l; i <= r; ++i) {
+                sum += arr[i];
+            }
+        }
+        else {
+            for (int i = l; i < blockSize * (lblk + 1); ++i) {
+                sum += arr[i];
+            }
+            for (int i = lblk + 1; i < rblk; ++i) {
+                sum += sumArr[i];
+            }
+            for (int i = blockSize * rblk; i <= r; ++i) {
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
+
+    ll rangeMax(int l, int r)
+    {
+        ll res = -0x7fffffffffffffff - 1;
+        int lblk = l / blockSize;
+        int rblk = r / blockSize;
+    
+        if (lblk == rblk) {
+            for (int i = l; i <= r; ++i) {
+                res = max(res, arr[i]);
+            }
+        }
+        else {
+            for (int i = l; i < blockSize * (lblk + 1); ++i) {
+                res = max(res, arr[i]);
+            }
+            for (int i = lblk + 1; i < rblk; ++i) {
+                res = max(res, maxArr[i]);
+            }
+            for (int i = blockSize * rblk; i <= r; ++i) {
+                res = max(res, arr[i]);
+            }
+        }
+        return res;
+    }
+
+    void update(int idx, ll x)
+    {
+        sumArr[idx / blockSize] = sumArr[idx / blockSize] - arr[idx] + x;
+        maxArr[idx / blockSize] = max(x, maxArr[idx / blockSize]);
+        arr[idx] = x;
     }
 };
