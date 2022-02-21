3e1408d4e3525bfbec5473b80ff11d4009d501d4
Rudra Pratap Singh <99959625+chainsn4tchers@users.noreply.github.com>
2022-02-22 01:44:51 +0530

    Update segtree_lt.cpp

--- segtree_lt.cpp
+++ segtree_lt.cpp
@@ -20,9 +20,9 @@ struct segtree {
 
     void build(vector<ll>& a)
     {
-	size = 4 * a.size();
-	tree.assign(size + 1, 0);
-	init(0, sz(a) - 1, 0, a);
+        size = 4 * a.size();
+        tree.assign(size + 1, 0);
+        init(0, sz(a) - 1, 0, a);
     }
 
     ll rangeSum(int lo, int hi, int idx, int l, int r)
@@ -32,5 +32,19 @@ struct segtree {
             return tree[idx];
         int mid = lo + ((hi - lo) / 2);
         return rangeSum(lo, mid, (2 * idx) + 1, l, min(r, mid)) + rangeSum(mid + 1, hi, (2 * idx) + 2, max(l, mid + 1), r);
+    }
+
+    void update(int lo, int hi, int idx, int pos, ll x)
+    {
+        if (lo == hi) {
+            tree[lo] = x;
+            return;
+        }
+        int mid = lo + ((hi - lo) / 2);
+        if (pos <= mid)
+            update(lo, mid, (2 * idx) + 1, pos, x);
+        else
+            update(mid + 1, hi, (2 * idx) + 2, pos, x);
+        tree[idx] = tree[(2 * idx) + 1] + tree[(2 * idx) + 2];
     }
 };
